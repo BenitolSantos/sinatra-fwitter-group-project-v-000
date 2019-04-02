425fda00fff618a7c3aa4dacbc1ad71037f08b32
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-02 15:23:57 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -70,6 +70,7 @@ class TweetsController < ApplicationController #ApplicationController inheritanc
     delete '/tweets/:id' do
       if logged_in?
         @tweet = Tweet.find_by(id: params[:id])
+        binding.pry
         User.find_by(id: params[:user_id])
         current_user.tweets.delete(@tweet)
         Tweet.all.delete(@tweet)

f81def9739b2f18fd177285e23ce8d3da6f02fa1
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-02 15:27:37 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -71,6 +71,8 @@ class TweetsController < ApplicationController #ApplicationController inheritanc
       if logged_in?
         @tweet = Tweet.find_by(id: params[:id])
         @user = User.find_by(id: @tweet.user_id)
+        if user
+          #delete action logged in does not let a user delete a tweet they did not create
         @user.tweets.delete(@tweet)
         Tweet.all.delete(@tweet)
         redirect to("/tweets")
